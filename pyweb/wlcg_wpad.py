041f7e0fb6fed1eab3da47fe82bd1863462d55d7
Dave Dykstra <dwd@fnal.gov>
2016-04-13 17:19:19 -0500

    change to be wlcg-wpad server instead of dispatcher

--- pyweb/wlcg_wpad.py
+++ pyweb/wlcg_wpad.py
@@ -1,24 +1,4 @@
-# Dispatch a wpad.dat request
+# Find proxies for WLCG grid site
 
-def error_request(start_response, response_code, response_body):
-    response_body = response_body + '\n'
-    start_response(response_code,
-                   [('Content-Length', str(len(response_body)))])
-    return [response_body]
-
-def bad_request(start_response, reason):
-    response_body = 'Bad Request: ' + reason
-    return error_request(start_response, '400 Bad Request', response_body)
-
-def good_request(start_response, response_body):
-    response_code = '200 OK'
-    start_response(response_code,
-                  [('Content-Type', 'text/plain'),
-                   ('Content-Length', str(len(response_body)))])
-    return [response_body]
-
-
-def dispatch(environ, start_response):
-    body = str(environ)
-    return good_request(start_response, body)
-
+def get_proxies(remoteip):
+    return ""
