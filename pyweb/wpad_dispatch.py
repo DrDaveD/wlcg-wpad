e326397e937a7270b010e796cf86617543481a6d
Dave Dykstra <dwd@fnal.gov>
2016-09-29 17:09:51 -0500

    change to using wlcgwpad.conf, read any keyword, support recursive values, support WLCG special "proxy"

--- pyweb/wpad_dispatch.py
+++ pyweb/wpad_dispatch.py
@@ -1,12 +1,12 @@
 # Dispatch a wpad.dat request
 # If the URL ends with ?ip=ADDR then use ADDR instead of $REMOTE_ADDR
 
-import sys, urlparse
+import sys, urlparse, copy
 import wlcg_wpad
 from wpad_utils import *
 import geosort
 
-geosortconffile = '/var/lib/wlcg-wpad/geosort.conf'
+wlcgwpadconffile = '/var/lib/wlcg-wpad/wlcgwpad.conf'
 
 def error_request(start_response, response_code, response_body):
     response_body = response_body + '\n'
@@ -28,21 +28,44 @@ def good_request(start_response, response_body):
                    ('Content-Length', str(len(response_body)))])
     return [response_body]
 
-def parse_geosort_conf():
-    hostproxies = []
-    try:
-	for line in open(geosortconffile, 'r').read().split('\n'):
-	    words = line.split()
-	    if words:
-		if words[0] == 'hostproxies':
-		    parts = words[1].split('=')
-		    proxies = parts[1].split(',')
-		    hostproxies.append([parts[0], proxies])
-    except Exception, e:
-	logmsg('-', '-', 'error reading ' + geosortconffile + ', continuing: ' + str(e))
-    return hostproxies
+conf = {}
 
-hostproxies = parse_geosort_conf()
+def parse_wlcgwpad_conf():
+    global conf
+    try:
+	for line in open(wlcgwpadconffile, 'r').read().split('\n'):
+            line = line.split('#',1)[0]  # removes comments
+	    words = line.split(None,1)
+	    if len(words) < 2:
+                continue
+            key = words[0]
+            parts = words[1].split('=')
+            if len(parts) == 0:
+                continue
+            if key not in conf:
+                conf[key] = {}
+            name = parts[0]
+            if len(parts) == 1:
+                # there was no '='; enter name as an empty list
+                conf[key][name] = []
+                continue
+            values = parts[1].split(',')
+            idx = 0
+            while idx < len(values):
+                value = values[idx]
+                if value in conf[key]:
+                    # replace value with list from previously defined name
+                    newvalues = conf[key][value]
+                    values[idx:idx+1] = newvalues
+                    idx += len(newvalues)
+                else:
+                    idx += 1
+            conf[key][name] = values
+    except Exception, e:
+	logmsg('-', '-', 'error reading ' + wlcgwpadconffile + ', continuing: ' + str(e))
+    return
+
+parse_wlcgwpad_conf()
 
 # return a proxy auto config statement that returns a list of proxies
 def getproxystr(proxies):
@@ -69,25 +92,24 @@ def dispatch(environ, start_response):
 	    remoteip = parameters['ip'][0]
     msg = None
     wpadinfo = {}
-    if host == 'wlcg-wpad.cern.ch':
-	wpadinfo = wlcg_wpad.get_proxies(host, remoteip)
-        if 'msg' in wpadinfo:
-            msg = wpadinfo['msg']
-	if 'proxies' not in wpadinfo:
-	    return bad_request(start_response, host, remoteip, str(msg))
+    if ('hostproxies' in conf) and (host in conf['hostproxies']):
+        hostproxies = copy.copy(conf['hostproxies'][host])
+        if hostproxies[0] == 'WLCG':
+            wpadinfo = wlcg_wpad.get_proxies(host, remoteip)
+            if 'msg' in wpadinfo:
+                msg = wpadinfo['msg']
+            if 'proxies' not in wpadinfo:
+                if len(hostproxies) == 1:
+                    return bad_request(start_response, host, remoteip, str(msg))
+                del hostproxies[0]
+                logmsg(host, remoteip, 'WLCG proxy not found, falling back')
+        if 'proxies' not in wpadinfo:
+            proxies, msg = geosort.sort_proxies(remoteip, hostproxies)
+            if proxies == []:
+                return bad_request(start_response, host, remoteip, msg)
+            wpadinfo['proxies'] = [{'default' : proxies}]
     else:
-        gotone = False
-        for hostproxy in hostproxies:
-            if host == hostproxy[0]:
-                gotone = True
-                proxies, msg = geosort.sort_proxies(remoteip, hostproxy[1])
-                if proxies == []:
-                    return bad_request(start_response, host, remoteip, msg)
-		break
-        if not gotone:
-            return bad_request(start_response, host, remoteip, 'Unrecognized host name')
-	logmsg(host, remoteip, 'squids are ' + ';'.join(proxies))
-        wpadinfo['proxies'] = [{'default' : proxies}]
+        return bad_request(start_response, host, remoteip, 'Unrecognized host name')
 
     proxies = []
     balance = False
