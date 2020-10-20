efa50851dbfad8c4f9152c6e5a8e542bceb80317
Edita Kizinevic <edita.kizinevic@cern.ch>
2020-10-20 14:25:50 +0200

    Avoid crashing when organization is None

--- pyweb/wpad_utils.py
+++ pyweb/wpad_utils.py
@@ -1,4 +1,4 @@
 import os
 
 def logmsg(host, ip, org, msg):
-    print host + ' ' + ip + ' [' + org.encode('ISO-8859-1') + '] ' + msg
+    print host + ' ' + ip + ' [' + str(org).encode('ISO-8859-1') + '] ' + msg
