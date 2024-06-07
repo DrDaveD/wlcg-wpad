2f65bfdb67f5cc4102429bfb6be8f03c7ff375db
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2024-06-07 14:28:08 -0500

    do not encode logmsg

--- pyweb/wpad_utils.py
+++ pyweb/wpad_utils.py
@@ -1,5 +1,4 @@
 import os
 
 def logmsg(host, ip, org, msg):
-    full_msg = host + ' ' + ip + ' [' + str(org) + '] ' + msg
-    print(full_msg.encode('ISO-8859-1'))
+    print(host + ' ' + ip + ' [' + str(org) + '] ' + msg)
